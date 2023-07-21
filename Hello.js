d02af9bcff5c5c6314c976e426cb8e143404f0d9
Vidhi <srivastavavidhi29@gmail.com>
2023-07-21 17:37:57 +0530

    Added Javscript file

---
+++ Hello.js
@@ -0,0 +1,10 @@
+function sayHello(){
+    const name = document.getElementById('name').value;
+    if (name.trim() === '') {
+        alert('Please enter your name before clicking the button.');
+    } 
+    else {
+        const messageElement = document.getElementById('message');
+        messageElement.innerText = 'Hello, ' + name + '!!!';
+    }
+}
